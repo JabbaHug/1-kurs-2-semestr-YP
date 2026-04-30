6e50db11483e3d7e2ab29d20ebcc97d8661beb55
Иван Янчев <136200955+JabbaHug@users.noreply.github.com>
2026-04-30 17:08:24 +0300

    Update Tetrahedron.h

--- Tetrahedron.h
+++ Tetrahedron.h
@@ -2,60 +2,46 @@
 #define TETRAHEDRON_H
 
 /**
+ * @struct Point
+ * @brief Точка в трёхмерном пространстве
+ */
+struct Point {
+    double x;
+    double y;
+    double z;
+};
+
+/**
  * @class Tetrahedron
- * @brief Класс, описывающий геометрический тетраэдр.
- * Тетраэдр задаётся координатами четырёх точек в пространстве.
+ * @brief Класс, описывающий тетраэдр
  */
 class Tetrahedron {
 private:
-    /**
-     * @brief Координаты первой точки
-     */
-    double x1, y1, z1;
+    Point A, B, C, D;
 
     /**
-     * @brief Координаты второй точки
-     */
-    double x2, y2, z2;
-
-    /**
-     * @brief Координаты третьей точки
-     */
-    double x3, y3, z3;
-
-    /**
-     * @brief Координаты четвёртой точки
-     */
-    double x4, y4, z4;
-
-    /**
-     * @brief validate - проверка корректности данных
+     * @brief Проверяет корректность тетраэдра (точки не должны лежать в одной плоскости)
      */
     void validate() const;
 
 public:
     /**
-     * @brief Конструктор класса Tetrahedron.
-     * @param ax, ay, az Координаты первой точки
-     * @param bx, by, bz Координаты второй точки
-     * @param cx, cy, cz Координаты третьей точки
-     * @param dx, dy, dz Координаты четвёртой точки
+     * @brief Конструктор
+     * @param A первая точка
+     * @param B вторая точка
+     * @param C третья точка
+     * @param D четвёртая точка
      */
-    Tetrahedron(double ax, double ay, double az,
-                double bx, double by, double bz,
-                double cx, double cy, double cz,
-                double dx, double dy, double dz);
+    Tetrahedron(const Point& A,
+                const Point& B,
+                const Point& C,
+                const Point& D);
 
     /**
-     * @brief Вычисляет объём тетраэдра.
-     * @return Объём
+     * @brief Вычисляет объём тетраэдра
+     * @return объём
      */
     double volume() const;
-
-    /**
-     * @brief Выводит информацию о тетраэдре в консоль.
-     */
-    void print() const;
 };
 
 #endif
